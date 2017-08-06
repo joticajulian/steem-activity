b08fae3ac23920b3fa77a785173558cba152d93e
joticajulian <joticajulian@gmail.com>
2017-08-06 09:23:08 -0500

    support to multilanguage

---
+++ js/multilanguages.js
@@ -0,0 +1,57 @@
+var label = {};
+label['en'] = {};
+label['es'] = {};
+label['fr'] = {};
+
+//IDs in index.html
+
+label["en"]["createdby"]="Created by";
+label["es"]["createdby"]="Creado por";
+
+label["en"]["input-account"]="Account";
+label["es"]["input-account"]="Cuenta";
+
+label["en"]["title-activity-account"]="Account activity";
+label["es"]["title-activity-account"]="Actividad de la cuenta";
+
+label["en"]["lastweeks"]="Last 4 weeks";
+label["es"]["lastweeks"]="Últimas 4 semanas";
+
+label["en"]["title-activity-followers"]="Account activity";
+label["es"]["title-activity-followers"]="Actividad de los seguidores";
+
+label["en"]["title-followers"]="Followers";
+label["es"]["title-followers"]="Seguidores";
+
+label["en"]["lastweeks2"]="Last 4 weeks";
+label["es"]["lastweeks2"]="Últimas 4 semanas";
+
+label["en"]["steemactivityby"]='Steem activity by <a href="https://steemit.com/@jga/">@jga</a>';
+label["es"]["steemactivityby"]='Steem activity por <a href="https://steemit.com/@jga/">@jga</a>';
+
+label["en"]["seeitongithub"]='See it on <a href="https://github.com/joticajulian/steem-activity/">github.com</a> and <a href="https://www.steem.center/index.php?title=Steem_Ecosystem:S-Steemis">steem.center</a>';
+label["es"]["seeitongithub"]='Míralo en <a href="https://github.com/joticajulian/steem-activity/">github.com</a> y también en <a href="https://www.steem.center/index.php?title=Steem_Ecosystem:S-Steemis">steem.center</a>';
+
+//labels in steem_activity.js
+
+label["en"]["Local time: "]="Local time: ";
+label["es"]["Local time: "]="Tiempo local: ";
+
+label["en"]["nameDay"]=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+label["es"]["nameDay"]=['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'];
+
+label["en"]["Followers of @"]="Followers of @";
+label["es"]["Followers of @"]="Seguidores de @";
+
+label["en"][" votes"]=" votes";
+label["es"][" votes"]=" votos";
+
+label["en"][". Complete"]=". Complete";
+label["es"][". Complete"]=". Completo";
+
+label["en"]["Best Time #"]="Best Time #";
+label["es"]["Best Time #"]="Mejor tiempo #";
+
+label["en"][" at "]=" at ";
+label["es"][" at "]=" a las ";
+
