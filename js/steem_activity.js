f13915dd34a1dcd71ef9de4c210fe60a98347680
Julian <joticajulian@gmail.com>
2018-01-17 09:59:53 -0500

    bug solved: abort last call when the number of weeks is changed

--- js/steem_activity.js
+++ js/steem_activity.js
@@ -3,6 +3,8 @@ load_language();
 
 var now = new Date();
 var g = 0;
+var tBar;
+var tPer;
 var gmt = now.getTimezoneOffset()*60*1000; //gmt in milliseconds
 var last1week = new Date(now.getFullYear(),now.getMonth(),now.getDate()-6);
 var last2week = new Date(now.getTime() - 2*7*24*60*60*1000);
@@ -11,15 +13,15 @@ var last4week = new Date(now.getTime() - 4*7*24*60*60*1000);
 
 $('.timezone').text(lang("Local time: ") + gmtToString());
 
-//Array of Votes and initialization
-
-
-
-
-
+var followersLoadedSubgroup = 0;
 var followersLoaded = 0;
 var totalFollowers = 0;
 var textFollowers = "";
+var sizeF = 0;
+var numberRequests = 20;
+var followers = new Array(1000);
+var account;
+var timeConsult;
 
 nameDay = lang("nameDay");
 nameHours = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23'];
@@ -30,46 +32,52 @@ var maxVotationDay = 0;
 var bestHour = [[0,0],[0,0],[0,0],[0,0]];
 var bestDay = 0;
 var refreshPlot = 10;
-var refreshText = 5;
+var refreshText = 1;
 var firstPlot = true;
 var firstText = true;
 
 var votesDay = [0,0,0,0,0,0,0];
 var votes = new Array(7);
 
+
 function loadFollowersActivity(){
 	steem.api.setOptions({ url: 'https://api.steemit.com'});
 
-	var account = getQuery();
+	account = getQuery();
     
     console.log("getting rewardFund(post)...");
     steem.api.getRewardFund("post", function(err, response){
+        console.log(err);
         var recent_claims = parseFloat(response.recent_claims);
         var reward_balance = parseFloat(response.reward_balance.replace(" STEEM",""));
           
         console.log("getting internal price...");
         steem.api.getCurrentMedianHistoryPrice(function(err, response){
+            console.log(err);
             var price = parseFloat(response.base.replace(" SBD",""))/parseFloat(response.quote.replace(" STEEM",""));
             g = reward_balance/recent_claims*price;
             
-            loadAccountProfile(account);
-            consultVotesAccount(account);
-            startConsultVotesFollowers(account);
+            loadAccountProfile();
+            consultVotesAccount();
+            startConsultVotesFollowers();
 	
             $('#sel1').change(function(){
-                consultVotesAccount(account);
+                consultVotesAccount();
             });
             
             $('#sel2').change(function(){
-                startConsultVotesFollowers(account);
+                startConsultVotesFollowers();
             });
         });
     });
 }
 
-function loadAccountProfile(account){
+function loadAccountProfile(){
     steem.api.getAccounts([account], function(err, result){
-        console.log(err, result);
+        if(err){
+            console.log(err);
+            return;
+        }
         var info = JSON.parse(result[0].json_metadata);
         if(info.profile.hasOwnProperty('profile_image')){
             console.log(info.profile.profile_image + " extracted: " + extractUrlProfileImage(info.profile.profile_image));
@@ -79,11 +87,11 @@ function loadAccountProfile(account){
         }
     });
     $('#labelAccount').text("@" + account);
-    $('#title-followers').text(lang("Followers of @") + account);
+    $('#title-followers').text(lang("Followers of @") + account);    
 }
 
 
-function consultVotesAccount(account){
+function consultVotesAccount(){
 
     var a_votesDay = [0,0,0,0,0,0,0];
     var a_votes = new Array(7);
@@ -94,34 +102,38 @@ function consultVotesAccount(account){
         }
     }
 
-	steem.api.getAccountVotes(account, function(err, result) {
     
+    steem.api.getAccountVotes(account, function(err, result) {
+        if(err){
+            console.log(err);
+            return;
+        }
         //look each vote and put it in the array regarding the time
-		for(i=result.length-1;i>=0;i--){
-			var timeUTC = new Date(result[i].time);
-			var time = new Date(timeUTC.getTime() - gmt);
-			if(timeToBreak("1",time,result.length-i)) break;					
-			var hours = time.getHours();
-			var day = time.getDay();
+        for(i=result.length-1;i>=0;i--){
+            var timeUTC = new Date(result[i].time);
+            var time = new Date(timeUTC.getTime() - gmt);
+            if(timeToBreak("1",time,result.length-i)) break;					
+            var hours = time.getHours();
+            var day = time.getDay();
             var vote = Number((parseInt(result[i].rshares)*g).toFixed(3));
-			a_votes[day][hours] += vote;
-			a_votesDay[day] += vote;	
-		}
+            a_votes[day][hours] += vote;
+            a_votesDay[day] += vote;	
+        }
 
-		for(i=0;i<7;i++){
-			var data = [{
-				x: nameHours,
-				y: a_votes[i],	      
-				type: 'bar'
-			}];					
-			var layout = { title: nameDay[i] };
-			Plotly.newPlot('a-chart'+i, data,layout);
-			$('#a-votesDay'+i).text(nameDay[i]+": $"+a_votesDay[i].toFixed(2));
-		}			
-	});
+        for(i=0;i<7;i++){
+            var data = [{
+                x: nameHours,
+                y: a_votes[i],	      
+                type: 'bar'
+            }];					
+            var layout = { title: nameDay[i] };
+            Plotly.newPlot('a-chart'+i, data,layout);
+            $('#a-votesDay'+i).text(nameDay[i]+": $"+a_votesDay[i].toFixed(2));
+        }			
+    });    
 }
 
-function startConsultVotesFollowers(account){
+function startConsultVotesFollowers(){
 
     votesDay = [0,0,0,0,0,0,0];
     votes = new Array(7);
@@ -135,97 +147,127 @@ function startConsultVotesFollowers(account){
     followersLoaded = 0;
     totalFollowers = 0;
     textFollowers = "";
-
-
-    steem.api.getFollowCount(account, function(err,result) {
+    
+    timeConsult = new Date();
+    
+    steem.api.getFollowCount(account, function(err,result) {        
+        if(err){
+            console.log(err);
+            return;
+        }
         console.log("Number of followers: "+result.follower_count);
         totalFollowers = result.follower_count;
         $('#title-followers').text(lang("Followers of @") + account + ": "+totalFollowers);
-        consultVotesFollowers(account,0);		
-    });
+        consultVotesFollowers(0,this.tC);
+    }.bind({tC:timeConsult}),timeConsult);    
 }
 
-function consultVotesFollowers(account, fromFollower){	
-	steem.api.getFollowers(account, fromFollower, 0, 21, function(err, result) {
-				
-		//look each for each Follower
-		var finish = result.length-1;
-		if(result.length == 1) finish=1; //last follower
-		
-		for(i=0;i<finish;i++){
-			if(result[i].what[0] != 'blog') continue;
-			textFollowers = textFollowers + link(result[i].follower) + " . . . . ";		
-									
-			steem.api.getAccountVotes(result[i].follower, function(err, result) {
-				//if (err) //error
-				//look each vote and put it in the array regarding the time
-				for(i=result.length-1;i>=0;i--){
-					var timeUTC = new Date(result[i].time);
-					var time = new Date(timeUTC.getTime() - gmt);
-					if(timeToBreak("2",time,result.length-i)) break;					
-					var hours = time.getHours();
-					var day = time.getDay();                    
-                    var vote = Number((parseInt(result[i].rshares)*g).toFixed(3));
-                    votes[day][hours] += vote;
-                    votesDay[day] += vote;	
+function consultVotesFollowers(fromFollower,tC){
+    steem.api.getFollowers(account, fromFollower, 'blog', followers.length, function(err, result) {
+        if(err){
+            console.log(err);
+            return;
+        }
+        sizeF = result.length;
+        for(i=0;i<sizeF;i++){
+            followers[i] = result[i].follower;
+            textFollowers = textFollowers + link(followers[i]) + " . . . . ";
+        }
+         
+        followersLoadedSubgroup = 0;
+        for(i=0;i<numberRequests;i++){
+            if(i<sizeF-1){ //sizeF-1: no not get the last one (this for the other round)
+                getVotesFollower(i,this.tC);                
+            }    
+        }
+        
+    }.bind({tC:tC}),tC);
+}
+
+function getVotesFollower(k,tC){
+    steem.api.getAccountVotes(followers[k], function(err, result) {
+        if(timeConsult != this.tC){
+            console.log("Aborting last call");
+            return;
+        }
+        
+        if(err){
+            console.log("Error follower: "+ followers[k]);
+            getVotesFollower(k,this.tC);
+            return;
+        }
+                        
+        //look each vote and put it in the array regarding the time
+        for(i=result.length-1;i>=0;i--){
+            var timeUTC = new Date(result[i].time);
+            var time = new Date(timeUTC.getTime() - gmt);
+            if(timeToBreak("2",time,result.length-i)) break;					
+            var hours = time.getHours();
+            var day = time.getDay();                    
+            var vote = Number((parseInt(result[i].rshares)*g).toFixed(3));
+            votes[day][hours] += vote;
+            votesDay[day] += vote;	
                     
-					for(j=0;j<4;j++){
-						if(votes[day][hours] > maxVotation[j]){
-							maxVotation[j] = votes[day][hours];
-							bestHour[j] = [day,hours];
-							break;
-						}
-					}
-					if(votesDay[day] > maxVotationDay){
-						maxVotationDay = votesDay[day];
-						bestDay = day;
-					}
-				}
+            for(j=0;j<4;j++){
+                if(votes[day][hours] > maxVotation[j]){
+                    maxVotation[j] = votes[day][hours];
+                    bestHour[j] = [day,hours];
+                    break;
+                }
+            }
+            if(votesDay[day] > maxVotationDay){
+                maxVotationDay = votesDay[day];
+                bestDay = day;
+            }
+        }
 
-				followersLoaded++;				
+        followersLoaded++;	
+        followersLoadedSubgroup++; 
 
-				refreshText--;
-				if(firstText || refreshText==0 || followersLoaded==totalFollowers){
-					var percentage = 100*followersLoaded/totalFollowers;
-					var tPer = percentage.toFixed(2);
-					var tBar = followersLoaded + "/" + totalFollowers;
-					if(followersLoaded == totalFollowers) tBar = tBar + lang(". Complete");
-					$('#progress-bar').text(tBar).attr('aria-valuenow', tPer).css('width',tPer+'%');
-					$('#followers').html(textFollowers);
-					for(i=0;i<4;i++){
-						$('#bestTime'+i).text(lang("Best Time #")+(i+1)+": " + nameDay[bestHour[i][0]] + lang(" at ") + bestHour[i][1] + " H");
-					}
-					firstText = false;
-					refreshText = 5;
-				}
+        //Refresh progress bar
+        refreshText--;
+        if(firstText || refreshText==0 || followersLoaded==totalFollowers){
+            var percentage = 100*followersLoaded/totalFollowers;
+            tPer = percentage.toFixed(2);
+            tBar = followersLoaded + "/" + totalFollowers;
+            //if(followersLoaded == totalFollowers) tBar = tBar + lang(". Complete");
+            $('#progress-bar').text(tBar).attr('aria-valuenow', tPer).css('width',tPer+'%');
+            $('#followers').html(textFollowers);
+            for(i=0;i<4;i++){
+                $('#bestTime'+i).text(lang("Best Time #")+(i+1)+": " + nameDay[bestHour[i][0]] + lang(" at ") + bestHour[i][1] + " H");
+            }
+            firstText = false;
+            refreshText = 1;
+        }
 				
-				refreshPlot--;
-				if(firstPlot || refreshPlot==0 || followersLoaded==totalFollowers){
-					for(i=0;i<7;i++){
-						var data = [{
-							x: nameHours,
-							y: votes[i],	      
-							type: 'bar'
-						}];					
-						var layout = { title: nameDay[i] };
-						Plotly.newPlot('f-chart'+i, data,layout);
-						$('#f-votesDay'+i).text(nameDay[i]+": $"+votesDay[i].toFixed(2));
-					}
-					firstPlot = false;
-					refreshPlot = 10;
-				}
-			});			
-		}
-		
-		if(result.length>1){
-			var lastFollower = result[result.length-1].follower;
-			console.log("Consult from "+lastFollower);
-			consultVotesFollowers(account, lastFollower);
-		}else{
-			console.log("Finished");
-		}
-	});
+        //Refresh plots
+        refreshPlot--;
+        if(firstPlot || refreshPlot==0 || followersLoaded==totalFollowers){
+            for(i=0;i<7;i++){
+                var data = [{
+                    x: nameHours,
+                    y: votes[i],
+                    type: 'bar'
+                }];					
+                var layout = { title: nameDay[i] };
+                Plotly.newPlot('f-chart'+i, data,layout);
+                $('#f-votesDay'+i).text(nameDay[i]+": $"+votesDay[i].toFixed(2));
+            }
+            firstPlot = false;
+            refreshPlot = 10;
+        }
+        
+        if(timeConsult == this.tC){
+            var next = k+numberRequests;
+            if(next<sizeF) getVotesFollower(next,this.tC);
+            if(followersLoadedSubgroup == sizeF) consultVotesFollowers(followers[sizeF-1],this.tC);
+        }else{
+            console.log("Aborting call");            
+        }        
+    }.bind({tC:tC}),tC);    
 }
+		
+        
 
 function timeToBreak(block,time,n){
     var selTime = $('#sel'+block).find(":selected").text();
